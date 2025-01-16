88f7d97c9c4f9a9fce4b4972928b5aac0d9f8340
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-16 19:31:20 +0200

    Continued with authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,10 @@
 import Joi from "joi";
 import User from "../models/userModel";
 import { hashPassword } from "../utils/passwordHelper";
-import { generateAccessToken } from "../utils/generateTokens";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../utils/generateTokens";
 
 const registerUserSchema = Joi.object({
   firstName: Joi.string()
@@ -22,6 +25,16 @@ const registerUserSchema = Joi.object({
     .min(8)
     .required(),
   role: Joi.string().required().default("user"),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    )
+    .min(8)
+    .required(),
 });
 
 const registerUser = async (req, res) => {
@@ -52,9 +65,37 @@ const registerUser = async (req, res) => {
 
     await newUser.save();
 
-    const accessToken = generateAccessToken();
+    const accessToken = generateAccessToken({
+      userId: newUser._id,
+      userRole: newUser.role,
+    });
+    const refreshToken = generateRefreshToken(
+      { userId: newUser._id, userRole: newUser.role },
+      newUser.tokenVersion
+    );
+    res
+      .status(201)
+      .json({ firstName, lastName, email, accessToken, refreshToken });
   } catch (error) {
     console.error("An error occurred while registering the user: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const {error, value} = loginSchema.validate(req.body, {
+      abortEarly: false
+    })
+    if (error){
+      const errorMessages = error.details.map(detail => detail.message)
+      return res.status(400).json({error: errorMessages})
+    }
+
+    const {email, password} = req.body
+    
+  } catch (error) {
+    console.error("An error occurred: ", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
