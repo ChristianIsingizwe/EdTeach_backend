d0e5cf340d44dfd2458dbdc45fdb9f23391625c8
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-17 05:20:44 +0200

    Dealing with authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 import User from "../models/userModel";
-import { hashPassword } from "../utils/passwordHelper";
+import { hashPassword, verifyPassword } from "../utils/passwordHelper";
 import {
   generateAccessToken,
   generateRefreshToken,
@@ -70,7 +70,7 @@ const registerUser = async (req, res) => {
       userRole: newUser.role,
     });
     const refreshToken = generateRefreshToken(
-      { userId: newUser._id, userRole: newUser.role },
+      { userId: await newUser._id },
       newUser.tokenVersion
     );
     res
@@ -84,16 +84,42 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   try {
-    const {error, value} = loginSchema.validate(req.body, {
-      abortEarly: false
-    })
-    if (error){
-      const errorMessages = error.details.map(detail => detail.message)
-      return res.status(400).json({error: errorMessages})
+    const { error, value } = loginSchema.validate(req.body, {
+      abortEarly: false,
+    });
+    if (error) {
+      const errorMessages = error.details.map((detail) => detail.message);
+      return res.status(400).json({ error: errorMessages });
     }
 
-    const {email, password} = req.body
-    
+    const { email, password } = req.body;
+
+    const user = User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = verifyPassword(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const accessToken = generateAccessToken({
+      userId: user._id,
+      userRole: user.role,
+    });
+    const refreshToken = generateRefreshToken(
+      { userId: user._id },
+      user.tokenVersion
+    );
+    res
+      .status(200)
+      .json({
+        lastName: user.lastName,
+        email: user.email,
+        accessToken,
+        refreshToken,
+      });
   } catch (error) {
     console.error("An error occurred: ", error);
     res.status(500).json({ message: "Internal server error" });
