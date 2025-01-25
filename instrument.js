7307fe8fe59c4d2265e849dd8049897651a70249
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-25 08:27:22 +0200

    Setting up sentry for error monitoring and tracing

--- instrument.js
+++ instrument.js
@@ -4,23 +4,17 @@ const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 Sentry.init({
   dsn: "https://c62bafff66f00a4a4c9dc3d02e3750ee@o4508686691008512.ingest.de.sentry.io/4508686717288528",
   integrations: [nodeProfilingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracesSampleRate: 1.0,
+  profileSampleRate: 1.0,
 });
-// Manually call startProfiler and stopProfiler
-// to profile the code in between
+
 Sentry.profiler.startProfiler();
 
-// Starts a transaction that will also be profiled
 Sentry.startSpan(
   {
     name: "My First Transaction",
   },
-  () => {
-    // the code executing inside the transaction will be wrapped in a span and profiled
-  }
+  () => {}
 );
 
-// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
-// your application until the process exits or stopProfiling is called.
 Sentry.profiler.stopProfiler();
